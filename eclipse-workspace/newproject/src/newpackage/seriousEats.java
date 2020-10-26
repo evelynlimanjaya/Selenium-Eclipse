73f1390b07d56ebd33f772d1844f4c50f04e7537
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-10-25 22:45:39 -0700

    Selenium practice

--- eclipse-workspace/newproject/src/newpackage/seriousEats.java
+++ eclipse-workspace/newproject/src/newpackage/seriousEats.java
@@ -11,17 +11,17 @@ public class seriousEats {
 		WebDriver driver=new ChromeDriver();
 
 		String baseUrl="https://www.seriouseats.com/";
-
-		
-		
+		String expectedSearch="Chinese Herbal Silkie Chicken Soup Recipe";
+		String actualSearch="";
 		
 		driver.get(baseUrl);
 		driver.manage().window().maximize();
-		WebElement myElement=driver.findElement(By.xpath("(//*[@class='icon icon-search-glass'])[2]"));		
-		myElement.click();
-//		driver.findElement(By.xpath("(//*[@class='icon icon-search-glass'])[2]")).click();
+		WebElement searchIcon=driver.findElement(By.xpath("(//*[@class='icon icon-search-glass'])[2]"));
+		WebElement searchInput=driver.findElement(By.id("search-box"));
+		searchIcon.click();
+		searchInput.sendKeys("Chinese Herbal Silkie Chicken Soup Recipe");
+		searchInput.sendKeys(Keys.ENTER);
 		Thread.sleep(10000);
-//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.close();
 		
 	}

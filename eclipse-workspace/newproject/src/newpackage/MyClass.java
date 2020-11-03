7d417b3c1b3924d3fdd7ff4293f83b2884c64fe4
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-02 18:54:23 -0800

    Selenium practice

--- eclipse-workspace/newproject/src/newpackage/MyClass.java
+++ eclipse-workspace/newproject/src/newpackage/MyClass.java
@@ -1,18 +1,18 @@
 package newpackage;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 //comment the above line and uncomment below line to use Chrome
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 public class MyClass {
 
 
     public static void main(String[] args) {
         // declaration and instantiation of objects/variables
-//    	System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
-//		WebDriver driver = new FirefoxDriver();
+    	System.setProperty("webdriver.gecko.driver","C:\\WebDriver\\geckodriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		//comment the above 2 lines and uncomment below 2 lines to use Chrome
 //		System.setProperty("webdriver.chrome.driver","C:\\WebDriver\\bin\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
     	
         String baseUrl = "http://demo.guru99.com/test/newtours/";
         String expectedTitle = "Welcome: Mercury Tours";

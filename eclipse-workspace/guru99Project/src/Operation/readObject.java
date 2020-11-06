85ff27ca83d2a500cce7e9a917bb4aa36617700d
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-05 22:33:32 -0800

    day 3 guru 99 challenge

---
+++ eclipse-workspace/guru99Project/src/Operation/readObject.java
@@ -0,0 +1,17 @@
+package Operation;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class readObject {
+	Properties p = new Properties();
+    public Properties getObjectRepository() throws IOException{
+        //Read object repository file
+        InputStream stream = new FileInputStream(new File("C:\\Users\\Eien\\Selenium-Eclipse\\eclipse-workspace\\guru99Project\\src\\objects\\object.properties"));
+        //load all objects
+        p.load(stream);
+         return p;
+    }
+}

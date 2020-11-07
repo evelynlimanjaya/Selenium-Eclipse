4255b4ea9cbf54cd23d6d8001e3d021f8c2d7bf3
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-06 21:28:09 -0800

    Keyword Driven Framework

--- eclipse-workspace/guru99Project/src/Operation/readObject.java
+++ eclipse-workspace/guru99Project/src/Operation/readObject.java
@@ -1,34 +1,17 @@
 package Operation;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class readObject {
-	String result = "";
-	InputStream inputStream;
- 
-	public String getPropValues() throws IOException {
- 
-		try {
-			Properties prop = new Properties();
-			String propFileName = "object.properties";
- 
-			inputStream = new FileInputStream(new File(System.getProperty("C:\\Users\\Eien\\Selenium-Eclipse\\eclipse-workspace\\guru99Project\\src\\objects\\object.properties")));
- 
-			if (inputStream != null) {
-				prop.load(inputStream);
-			} else {
-				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-			}
-		}
-		 catch (Exception e) {
-				System.out.println("Exception: " + e);
-			} finally {
-				inputStream.close();
-			}
-		return result;
-}
+	Properties p = new Properties();
+    public Properties getObjectRepository() throws IOException{
+        //Read object repository file
+        InputStream stream = new FileInputStream(new File("C:\\Users\\Eien\\Selenium-Eclipse\\eclipse-workspace\\guru99Project\\src\\objects\\object.properties"));
+        //load all objects
+        p.load(stream);
+         return p;
+    }
 }
---
+++ eclipse-workspace/keywordDriven/src/utility/Constants.java
@@ -0,0 +1,10 @@
+package utility;
+
+public class Constants {
+	public static final String URL = "https://www.browserstack.com/users/sign_in/"; 
+	//Mention the path to your excel file
+	public static final String filePath = "C:\\Users\\Eien\\Selenium-Eclipse\\eclipse-workspace\\keywordDriven\\src\\excelData\\DemoFile.xlsx"; 
+	public static final String excelData = "DemoFile.xlsx"; 
+	public static final String username = "Your_Username"; 
+	public static final String password = "Your_password"; 
+}

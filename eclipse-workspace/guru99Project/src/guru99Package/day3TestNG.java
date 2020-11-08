3b31d806c3169484c25b6600a8c4e77daa85398b
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-07 20:02:50 -0800

    day 3 read excel

--- eclipse-workspace/guru99Project/src/guru99Package/day3TestNG.java
+++ eclipse-workspace/guru99Project/src/guru99Package/day3TestNG.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import readExcel.readGuru99Excel;
 import org.testng.annotations.Test;
@@ -22,31 +23,33 @@ public class day3TestNG {
 		
 		driver.get(baseUrl);
 		driver.manage().window().maximize();
-		
-		WebElement UserID=driver.findElement(By.cssSelector("[name='uid']"));
-		WebElement Password=driver.findElement(By.cssSelector("[name='password']"));
-		WebElement Login=driver.findElement(By.cssSelector("[name='btnLogin']"));
+
 		
 		readGuru99Excel file=new readGuru99Excel();
 		
 		Sheet guru99Sheet = file.readExcel(System.getProperty("user.dir"),"guru99Excel.xlsx" , "Sheet1");
-		int rowCount = guru99Sheet.getLastRowNum()-guru99Sheet.getFirstRowNum();
+//		int rowCount = guru99Sheet.getLastRowNum()-guru99Sheet.getFirstRowNum();
 		for(int i=1;i<4;i++) {
+			WebElement UserID=driver.findElement(By.cssSelector("[name='uid']"));
+			WebElement Password=driver.findElement(By.cssSelector("[name='password']"));
+			WebElement Login=driver.findElement(By.cssSelector("[name='btnLogin']"));
 			Row row = guru99Sheet.getRow(i);
+
 			UserID.sendKeys(row.getCell(0).getStringCellValue());
 			Password.sendKeys(row.getCell(1).getStringCellValue());
 			Login.click();
 			WebElement titleSelector= driver.findElement(By.cssSelector("marquee[class='heading3']"));
 ////		wait.until(ExpectedConditions.visibilityOf(titleSelector));
-		String titleText=titleSelector.getText();
+			String titleText=titleSelector.getText();
 		
-		if(titleText.equals("Welcome To Manager's Page of Guru99 Bank")) {
+			if(titleText.equals("Welcome To Manager's Page of Guru99 Bank")) {
 			System.out.print("Test Passed");
-		}
-		else {
+			}
+			else {
 			System.out.print("Test Failed");
-		}
-		
+			}
+			driver.navigate().back();
+
 		}
 		
 //		 System.out.print(row.getCell(0).getStringCellValue()+"|| ");

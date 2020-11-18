c01584fbd4b9077247a020eb528bb8447891c97d
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-17 22:48:40 -0800

    ecommerce test

---
+++ eclipse-workspace/ecommerce/src/test/day1Test.java
@@ -0,0 +1,15 @@
+package test;
+import utility.constants;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class day1Test {
+	
+	@BeforeTest
+	
+	
+  @Test
+  public void nameSort() {
+  }
+}

a0f9fdd8d1d908478bd506f4a0d7f686202f7572
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-17 22:55:43 -0800

    ecommerce test

--- eclipse-workspace/ecommerce/src/test/day1Test.java
+++ eclipse-workspace/ecommerce/src/test/day1Test.java
@@ -1,15 +1,38 @@
 package test;
 import utility.constants;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class day1Test {
+	public WebDriver driver;
+	public WebDriverWait wait;
 	
 	@BeforeTest
+	public void launchBrowser() {
+		System.setProperty("webdriver.gecko.driver","C:\\WebDriver\\geckodriver\\geckodriver.exe");
+		ProfilesIni profile = new ProfilesIni();
+		FirefoxProfile myProfile = profile.getProfile("testProfile");
+		FirefoxOptions opt = new FirefoxOptions();
+		opt.setProfile(myProfile);
+		driver =  new FirefoxDriver(opt);
+		wait=new WebDriverWait(driver,5);
+		
+
+		
+		
+	}
 	
 	
-  @Test
-  public void nameSort() {
-  }
+	@Test
+	public void nameSort() {
+		driver.get(constants.URL);
+		driver.manage().window().maximize();
+	}
 }
--- eclipse-workspace/ecommerce/src/utility/constants.java
+++ eclipse-workspace/ecommerce/src/utility/constants.java
@@ -1,5 +1,6 @@
 package utility;
 
 public class constants {
-
+public static final String URL="http://live.demoguru99.com/index.php/";
+public static final String filePath="";
 }

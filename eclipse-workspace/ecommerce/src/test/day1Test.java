9ac837fe0887e59a02f5d3c69270f6bd8e7a93bb
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-18 22:04:28 -0800

    ecommerce practice

--- eclipse-workspace/ecommerce/src/test/day1Test.java
+++ eclipse-workspace/ecommerce/src/test/day1Test.java
@@ -3,6 +3,8 @@ import utility.constants;
 
 import static org.testng.Assert.assertEquals;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +13,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.ProfilesIni;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -19,6 +22,7 @@ import org.openqa.selenium.support.ui.Select;
 public class day1Test {
 	public WebDriver driver;
 	public WebDriverWait wait;
+	 private StringBuffer verificationErrors = new StringBuffer();
 	
 	@BeforeTest
 	public void launchBrowser() {
@@ -30,6 +34,8 @@ public class day1Test {
 		driver =  new FirefoxDriver(opt);
 		wait=new WebDriverWait(driver,5);
 		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
 		driver.get(constants.URL);
 		driver.manage().window().maximize();
 		
@@ -39,8 +45,12 @@ public class day1Test {
 	
 	@Test
 	public void verifyHomePage() {
-		String pageTitle=driver.getTitle();
-		assertEquals(pageTitle,"Home page");
+//		String pageTitle=driver.getTitle();
+//		assertEquals(pageTitle,"Home page");
+		
+		String demoSite  = driver.findElement(By.cssSelector("h2")).getText();
+	    System.out.println(demoSite);
+	    AssertJUnit.assertEquals("gfhfg", demoSite);	
 	
 	}
 	
@@ -53,15 +63,15 @@ public class day1Test {
 		
 	}
 	
-	@Test
-	public void nameSort() throws InterruptedException {
-		Thread.sleep(2000);
-		WebElement nameSort=driver.findElement(By.xpath("(//*[@title='Sort By'])[1]"));
-		nameSort.click();
-//		Select drpSort=new Select(driver.findElement(By.xpath("(//*[@title='Sort By'])[1]")));
-//		drpSort.selectByVisibleText("Name");
-		Thread.sleep(2000);
-	}
+//	@Test
+//	public void nameSort() throws InterruptedException {
+//		Thread.sleep(2000);c
+//		WebElement nameSort=driver.findElement(By.xpath("(//*[@title='Sort By'])[1]"));
+//		nameSort.click();
+////		Select drpSort=new Select(driver.findElement(By.xpath("(//*[@title='Sort By'])[1]")));
+////		drpSort.selectByVisibleText("Name");
+//		Thread.sleep(2000);
+//	}
 	@AfterTest
 	public void terminateBrowser() {
 		driver.quit();

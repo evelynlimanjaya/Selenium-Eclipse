5bd32bfb9f8d91bd2eb593e08ee3ecfd8dcb7e2c
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-21 21:32:14 -0800

    day 3 ecommerce

--- eclipse-workspace/ecommerce/src/test/day3Test.java
+++ eclipse-workspace/ecommerce/src/test/day3Test.java
@@ -1,9 +1,43 @@
 package test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import utility.constants;
+
 public class day3Test {
-  @Test
-  public void f() {
-  }
+	public WebDriver driver;
+	public WebDriverWait wait;
+	
+	@BeforeTest
+	public void launchBrowser() {
+		System.setProperty("webdriver.gecko.driver","C:\\WebDriver\\geckodriver\\geckodriver.exe");
+		ProfilesIni profile = new ProfilesIni();
+		FirefoxProfile myProfile = profile.getProfile("testProfile");
+		FirefoxOptions opt = new FirefoxOptions();
+		opt.setProfile(myProfile);
+		driver =  new FirefoxDriver(opt);
+		wait=new WebDriverWait(driver,5);
+		
+		driver.get(constants.URL);
+		driver.manage().window().maximize();
+	
+	
+	}
+	
+    @Test
+    public void cartTest() {
+    	WebElement mobileTab=driver.findElement(By.xpath("//a[contains(text(),'Mobile')]"));
+		mobileTab.click();
+
+    }
 }

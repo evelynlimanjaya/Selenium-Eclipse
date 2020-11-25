7494f0b9e78abd0c9b392865288e20e6d6d77f7c
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-24 22:42:00 -0800

    day 4 ecommerce

--- eclipse-workspace/ecommerce/src/test/day4Test.java
+++ eclipse-workspace/ecommerce/src/test/day4Test.java
@@ -2,6 +2,9 @@ package test;
 
 import static org.testng.Assert.assertEquals;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -37,9 +40,66 @@ public class day4Test {
 	
 	}
 	
+//	@Test
+//	public void compareTest() throws InterruptedException {
+//    	WebElement mobileTab=driver.findElement(By.xpath("//a[contains(text(),'Mobile')]"));
+//		mobileTab.click();
+//		WebElement xperiaCompare=driver.findElement(By.xpath("(//*[@class='link-compare'])[1]"));
+//		xperiaCompare.click();
+//		WebElement iPhoneCompare=driver.findElement(By.xpath("(//*[@class='link-compare'])[2]"));
+//		iPhoneCompare.click();
+//		WebElement galaxyCompare=driver.findElement(By.xpath("(//*[@class='link-compare'])[3]"));
+//		galaxyCompare.click();
+//		WebElement compareBtn=driver.findElement(By.cssSelector("button[title='Compare']"));
+//		compareBtn.click();
+//		for (String handle : driver.getWindowHandles()) {
+//			driver.switchTo().window(handle);
+//		   	}
+//		
+//		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='top']/body/div[1]/div[1]/h1")));
+//		WebElement header=driver.findElement(By.xpath("//*[@id='top']/body/div[1]/div[1]/h1"));
+//		String strHead = ("COMPARE PRODUCTS");
+//	    String compHead = header.getText();	
+//	    System.out.println("compHead = "+compHead);
+//	    String popupMobile1 = driver.findElement(By.xpath("//h2/a[@title='Sony Xperia']")).getText();
+//	    String popupMobile2 = driver.findElement(By.xpath("//h2/a[@title='IPhone']")).getText(); 
+//	    String popupMobile3=driver.findElement(By.xpath("//h2/a[@title='Samsung Galaxy']")).getText(); 
+//	    System.out.println("popupMobile1 = "+popupMobile1);
+//	    System.out.println("popupMobile2 = "+popupMobile2);
+//	    System.out.println("popupMobile3 = "+popupMobile3);
+//	    try {	    	
+//	    	assertEquals(strHead, compHead);
+//		    } catch (Exception e) {
+//		    	e.printStackTrace();	    	
+//		    }	
+//	  
+//	    try {	    	
+//	    	assertEquals("SONY XPERIA", popupMobile1);
+//		    } catch (Exception e) {
+//		    	e.printStackTrace();	    	
+//		    }	
+//	
+//	    try {	    	
+//	    	assertEquals("IPHONE", popupMobile2);
+//		    } catch (Exception e) {
+//		    	e.printStackTrace();	    	
+//		    }	
+//	    
+//	    try {	    	
+//	    	assertEquals("SAMSUNG GALAXY", popupMobile3);
+//		    } catch (Exception e) {
+//		    	e.printStackTrace();	    	
+//		    }
+//	    driver.findElement(By.xpath("//button[@title='Close Window']")).click();
+//	    for (String handle : driver.getWindowHandles()) {
+//		    driver.switchTo().window(handle);
+//		    }	 
+//	    
+//	}
+	
 	@Test
-	public void compareTest() throws InterruptedException {
-    	WebElement mobileTab=driver.findElement(By.xpath("//a[contains(text(),'Mobile')]"));
+	public void compareTest2() {
+		WebElement mobileTab=driver.findElement(By.xpath("//a[contains(text(),'Mobile')]"));
 		mobileTab.click();
 		WebElement xperiaCompare=driver.findElement(By.xpath("(//*[@class='link-compare'])[1]"));
 		xperiaCompare.click();
@@ -49,49 +109,60 @@ public class day4Test {
 		galaxyCompare.click();
 		WebElement compareBtn=driver.findElement(By.cssSelector("button[title='Compare']"));
 		compareBtn.click();
-		for (String handle : driver.getWindowHandles()) {
-			driver.switchTo().window(handle);
-		   	}
 		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='top']/body/div[1]/div[1]/h1")));
-		WebElement header=driver.findElement(By.xpath("//*[@id='top']/body/div[1]/div[1]/h1"));
-		String strHead = ("COMPARE PRODUCTS");
-	    String compHead = header.getText();	
-	    System.out.println("compHead = "+compHead);
-	    String popupMobile1 = driver.findElement(By.xpath("//h2/a[@title='Sony Xperia']")).getText();
-	    String popupMobile2 = driver.findElement(By.xpath("//h2/a[@title='IPhone']")).getText(); 
-	    String popupMobile3=driver.findElement(By.xpath("//h2/a[@title='Samsung Galaxy']")).getText(); 
-	    System.out.println("popupMobile1 = "+popupMobile1);
-	    System.out.println("popupMobile2 = "+popupMobile2);
-	    System.out.println("popupMobile3 = "+popupMobile3);
-	    try {	    	
-	    	assertEquals(strHead, compHead);
-		    } catch (Exception e) {
-		    	e.printStackTrace();	    	
-		    }	
-	  
-	    try {	    	
-	    	assertEquals("SONY XPERIA", popupMobile1);
-		    } catch (Exception e) {
-		    	e.printStackTrace();	    	
-		    }	
-	
-	    try {	    	
-	    	assertEquals("IPHONE", popupMobile2);
-		    } catch (Exception e) {
-		    	e.printStackTrace();	    	
-		    }	
-	    
-	    try {	    	
-	    	assertEquals("SAMSUNG GALAXY", popupMobile3);
-		    } catch (Exception e) {
-		    	e.printStackTrace();	    	
-		    }
-	    driver.findElement(By.xpath("//button[@title='Close Window']")).click();
-	    for (String handle : driver.getWindowHandles()) {
-		    driver.switchTo().window(handle);
-		    }	 
-	    
+		String MainWindow=driver.getWindowHandle();		
+		
+        // To handle all new opened window.				
+            Set<String> s1=driver.getWindowHandles();		
+        Iterator<String> i1=s1.iterator();		
+        		
+        while(i1.hasNext()) {
+        	String ChildWindow=i1.next();		
+    		
+            if(!MainWindow.equalsIgnoreCase(ChildWindow)) {
+            	 // Switching to Child window
+                driver.switchTo().window(ChildWindow);
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='top']/body/div[1]/div[1]/h1")));
+        		WebElement header=driver.findElement(By.xpath("//*[@id='top']/body/div[1]/div[1]/h1"));
+        		String strHead = ("COMPARE PRODUCTS");
+        	    String compHead = header.getText();	
+        	    System.out.println("compHead = "+compHead);
+        	    String popupMobile1 = driver.findElement(By.xpath("//h2/a[@title='Sony Xperia']")).getText();
+        	    String popupMobile2 = driver.findElement(By.xpath("//h2/a[@title='IPhone']")).getText(); 
+        	    String popupMobile3=driver.findElement(By.xpath("//h2/a[@title='Samsung Galaxy']")).getText(); 
+        	    System.out.println("popupMobile1 = "+popupMobile1);
+        	    System.out.println("popupMobile2 = "+popupMobile2);
+        	    System.out.println("popupMobile3 = "+popupMobile3);
+        	    try {	    	
+        	    	assertEquals(strHead, compHead);
+        		    } catch (Exception e) {
+        		    	e.printStackTrace();	    	
+        		    }	
+        	  
+        	    try {	    	
+        	    	assertEquals("SONY XPERIA", popupMobile1);
+        		    } catch (Exception e) {
+        		    	e.printStackTrace();	    	
+        		    }	
+        	
+        	    try {	    	
+        	    	assertEquals("IPHONE", popupMobile2);
+        		    } catch (Exception e) {
+        		    	e.printStackTrace();	    	
+        		    }	
+        	    
+        	    try {	    	
+        	    	assertEquals("SAMSUNG GALAXY", popupMobile3);
+        		    } catch (Exception e) {
+        		    	e.printStackTrace();	    	
+        		    }
+        	    driver.findElement(By.xpath("//button[@title='Close Window']")).click();
+            	
+            }
+        }
+     // Switching to Parent window i.e Main Window.
+        driver.switchTo().window(MainWindow);
+		
 	}
 	
     @AfterTest

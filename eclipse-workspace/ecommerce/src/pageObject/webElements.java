eef634fcb0cf6d537094d0478bcd3d9070310d48
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-11-27 16:58:31 -0800

    day 6 ecommerce

--- eclipse-workspace/ecommerce/src/pageObject/webElements.java
+++ eclipse-workspace/ecommerce/src/pageObject/webElements.java
@@ -1,5 +1,17 @@
 package pageObject;
 
-public class webElements {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class webElements {
+	WebDriver driver;
+	By accountIcon=By.cssSelector("[class='skip-link skip-account']");
+	By myAccount=By.cssSelector("[title='My Account']");
+	By emailLogin=By.id("email");
+	By passLogin=By.id("pass");
+	By loginBtn=By.id("send2");
+	By wishlist=By.xpath("//a[contains(text(),'My Wishlist')]");
+	By addToCart=By.cssSelector("[title='Add to Cart']");
+	By checkout=By.cssSelector("[title='Proceed to Checkout']");
+	
 }

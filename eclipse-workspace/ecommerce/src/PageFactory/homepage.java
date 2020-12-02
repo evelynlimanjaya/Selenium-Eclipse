4e13ca01142f322b58cea83b7f165b635baeeea3
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-12-01 21:44:44 -0800

    RT Data Driven

--- eclipse-workspace/ecommerce/src/PageFactory/homepage.java
+++ eclipse-workspace/ecommerce/src/PageFactory/homepage.java
@@ -2,6 +2,12 @@ package PageFactory;
 
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindBy;
+
+import org.openqa.selenium.support.PageFactory;
+
 public class homepage {
 	WebDriver driver;
 	@FindBy();

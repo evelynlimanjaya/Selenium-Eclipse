ce4c90945517f49c00c5b4d36d5d7f2408530fe6
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-12-07 20:22:29 -0800

    Page Factory

--- eclipse-workspace/ecommerce/src/PageFactory/homepage.java
+++ eclipse-workspace/ecommerce/src/PageFactory/homepage.java
@@ -52,4 +52,6 @@ public class homepage {
 		checkout.click();
 	}
 	
+	
+	
 }
--- eclipse-workspace/ecommerce/src/test/day6Test.java
+++ eclipse-workspace/ecommerce/src/test/day6Test.java
@@ -18,6 +18,7 @@ public class day6Test {
 	private String email="crimson.eve@gmail.com";
 	private String password ="bobamilktea";
 
+	homepage objHomePage;
 	
 	  @BeforeTest
 	  public void launchBrowser() {
@@ -28,26 +29,27 @@ public class day6Test {
 	  
 	  @Test
 	  public void placeOrder() {
-			WebElement accountIcon=driver.findElement(By.cssSelector("[class='skip-link skip-account']"));
-			accountIcon.click();
-			WebElement myAccount=driver.findElement(By.cssSelector("[title='My Account']"));
-			myAccount.click();
-			WebElement emailLogin=driver.findElement(By.id("email"));
-			emailLogin.clear();
-			emailLogin.sendKeys(email);
-			WebElement passLogin=driver.findElement(By.id("pass"));
-			passLogin.clear();
-			passLogin.sendKeys(password);
-			WebElement loginBtn=driver.findElement(By.id("send2"));
-			loginBtn.click();
-			accountIcon.click();
-			WebElement wishlist=driver.findElement(By.xpath("//a[contains(text(),'My Wishlist')]"));
-			wishlist.click();
-			WebElement addToCart=driver.findElement(By.cssSelector("[title='Add to Cart']"));
-			addToCart.click();
-			WebElement checkout=driver.findElement(By.cssSelector("[title='Proceed to Checkout']"));
-			checkout.click();
+//			WebElement accountIcon=driver.findElement(By.cssSelector("[class='skip-link skip-account']"));
+//			accountIcon.click();
+//			WebElement myAccount=driver.findElement(By.cssSelector("[title='My Account']"));
+//			myAccount.click();
+//			WebElement emailLogin=driver.findElement(By.id("email"));
+//			emailLogin.clear();
+//			emailLogin.sendKeys(email);
+//			WebElement passLogin=driver.findElement(By.id("pass"));
+//			passLogin.clear();
+//			passLogin.sendKeys(password);
+//			WebElement loginBtn=driver.findElement(By.id("send2"));
+//			loginBtn.click();
+//			accountIcon.click();
+//			WebElement wishlist=driver.findElement(By.xpath("//a[contains(text(),'My Wishlist')]"));
+//			wishlist.click();
+//			WebElement addToCart=driver.findElement(By.cssSelector("[title='Add to Cart']"));
+//			addToCart.click();
+//			WebElement checkout=driver.findElement(By.cssSelector("[title='Proceed to Checkout']"));
+//			checkout.click();
 			
-			
+			objHomePage=new homepage(driver);
+			objHomePage.accessMyAccountTest();
 	  }
 }

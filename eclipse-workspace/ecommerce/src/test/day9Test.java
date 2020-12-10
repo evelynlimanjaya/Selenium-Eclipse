1dbbbdc20cb23e9d73de398a6f354a021e1cc340
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-12-09 21:20:31 -0800

    day 7 ecommerce

--- eclipse-workspace/ecommerce/src/test/day9Test.java
+++ eclipse-workspace/ecommerce/src/test/day9Test.java
@@ -1,8 +1,18 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import PageFactory.homepage;
+
 public class day9Test {
+	private WebDriver driver;
+	private WebDriverWait wait;
+	private String email="crimson.eve@gmail.com";
+	private String password ="bobamilktea";
+
+	homepage objHomePage;
   @Test
   public void f() {
   }

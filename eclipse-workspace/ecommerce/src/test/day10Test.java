c454aa5cbc4955afbaa423bcfdca63bba154b367
Evelyn Limanjaya <crimson.eve@gmail.com>
2020-12-12 22:30:02 -0800

    day 10 ecommerce

--- eclipse-workspace/ecommerce/src/test/day10Test.java
+++ eclipse-workspace/ecommerce/src/test/day10Test.java
@@ -1,8 +1,35 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import utility.constants;
+
 public class day10Test {
+	public WebDriver driver;
+	public WebDriverWait wait;
+	
+	@BeforeTest
+	public void launchBrowser() {
+		System.setProperty("webdriver.gecko.driver","C:\\WebDriver\\geckodriver\\geckodriver.exe");
+		ProfilesIni profile = new ProfilesIni();
+		FirefoxProfile myProfile = profile.getProfile("testProfile");
+		FirefoxOptions opt = new FirefoxOptions();
+		opt.setProfile(myProfile);
+		driver =  new FirefoxDriver(opt);
+		wait=new WebDriverWait(driver,5);
+		
+		driver.get(constants.URL);
+		driver.manage().window().maximize();
+	
+	
+	}
   @Test
   public void f() {
   }
